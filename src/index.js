594949cbe176c6edfff897ed02304a71a6814d73
Aleksei Morozov <aleksei.morozov@mera.com>
2020-03-03 11:56:49 +0300

    init: add brackets logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const symbolRegExp = /\W/;
+    const regExpStrBrackets = bracketsConfig.reduce((arrayBrackets, [open, close]) => {
+        let regExpBracket = '';
+        if (symbolRegExp.test(open)) {
+            regExpBracket += '\\';
+        }
+        regExpBracket += open;
+        if (symbolRegExp.test(open)) {
+            regExpBracket += '\\';
+        }
+        regExpBracket += close;
+
+        arrayBrackets.push(regExpBracket);
+
+        return arrayBrackets;
+    }, []).join('|');
+
+    let regExpBrackets = RegExp(regExpStrBrackets,'g');
+
+    let strBrackets = str;
+
+    while (regExpBrackets.test(strBrackets)) {
+        strBrackets = strBrackets.replace(regExpBrackets, '')
+    }
+
+    if (strBrackets.length) {
+        return false
+    }
+
+    return true;
 }
